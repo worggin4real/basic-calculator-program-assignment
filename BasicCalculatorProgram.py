87e33341ae175d31f02dc51aa9454020a8efd20d
Emmanuel Ocheore Ojirhevwe <33965274+worggin4real@users.noreply.github.com>
2025-04-16 14:05:45 +0000

    Add files via upload

---
+++ BasicCalculatorProgram.py
@@ -0,0 +1,22 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the mathematical operation (+, -, *, /): ")
+
+# Perform the operation and display the result
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. Please choose +, -, *, or /.")
